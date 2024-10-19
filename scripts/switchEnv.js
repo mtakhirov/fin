78702058152ca095a19e890225cad58d69ab73b0
Warnigo Warfurious <abumain0012@gmail.com>
2024-10-19 21:29:39 +0500

    fix: fixed script config in packages.json

--- scripts/switchEnv.js
+++ scripts/switchEnv.js
@@ -3,26 +3,33 @@ import fs from 'fs'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
+// Get __dirname for ES modules
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
+// Define paths for the environment files
 const projectRoot = path.join(__dirname, '..')
 const sourceEnvExample = path.join(projectRoot, '.env.example')
 const devFile = path.join(projectRoot, '.env.development')
 const prodFile = path.join(projectRoot, '.env.production')
 
+// Check if the source file exists
 if (!fs.existsSync(sourceEnvExample)) {
   console.log(chalk.red('Error: Source .env.example file does not exist.'))
   process.exit(1)
 }
 
+// Determine which argument is passed: --dev or --prod
 const argv = process.argv.slice(2)
 
 if (argv.includes('--dev')) {
-  copyFile(sourceEnvExample, devFile)
+  copyFile(sourceEnvExample, devFile) // Copy to .env.development for --dev
 } else if (argv.includes('--prod')) {
-  copyFile(sourceEnvExample, prodFile)
+  copyFile(sourceEnvExample, prodFile) // Copy to .env.production for --prod
 } else {
+  console.log(
+    chalk.red('Error: No valid argument provided. Please use --dev or --prod.'),
+  )
   process.exit(1)
 }
 
